0b1181f6299bec30f45cd8d05bbfb82ed364a380
הנדסת תוכנה <huser@sem.local>
2025-11-18 14:28:52 +0200

    wwwroot, change to 2 versions

---
+++ dotnet9/Models/Music.cs
@@ -0,0 +1,9 @@
+namespace MusicNameSpace.Models
+{
+    public class Music
+    {
+        public int Id{get;set;}
+        public string Name{get;set;}
+        public bool IsAccompanying{get;set;} 
+    }
+}
